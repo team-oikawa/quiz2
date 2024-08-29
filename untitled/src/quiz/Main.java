c61bbd32c974bb67fc2341789ca07bb8993c1a08
syougo <s.oouchida.sys23@morijyobi.ac.jp>
2024-08-29 12:37:06 +0900

    s1完成 close #11, close #12

---
+++ untitled/src/quiz/Main.java
@@ -0,0 +1,11 @@
+package quiz;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player player = new Player();
+
+        System.out.println("[結果]");
+        player.showCorrectQuizNum();
+    }
+}
---
+++ untitled/src/quiz/Player.java
@@ -0,0 +1,22 @@
+package quiz;
+
+public class Player {
+    private int correctQuizNum;
+    public Player() {
+        this.correctQuizNum = 0;
+    }
+
+    //正答数:3
+    public void showCorrectQuizNum() {
+        //TODO:実装する。
+        System.out.println("正答数:" + this.correctQuizNum);
+    }
+
+    public void setCorrectQuizNum(int correctQuizNum) {
+        this.correctQuizNum = correctQuizNum;
+    }
+
+    public int getCorrectQuizNum() {
+        return this.correctQuizNum;
+    }
+}
